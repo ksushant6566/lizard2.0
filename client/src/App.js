887075bfbd100fb0d28434bbaab03aed214d5859
sushant <ksushant6566@gmail.com>
2021-04-04 18:42:08 +0530

    auth context and header modification

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { AuthProvider } from './context/auth'
 
 import 'semantic-ui-css/semantic.min.css'
 import './App.css';
@@ -7,20 +8,24 @@ import { Container } from 'semantic-ui-react'
 
 // components
 import Header from './components/Header'
+
+// pages
 import Home from './pages/Home'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
 function App() {
   return (
-    <Router>
-      <Container>
-        <Header />
-        <Route exact path='/' component={Home} />
-        <Route exact path='/login' component={Login} />
-        <Route exact path='/register' component={Register} />
-      </Container>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Container>
+          <Header />
+          <Route exact path='/' component={Home} />
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Container>
+      </Router>
+    </AuthProvider>
   );
 }
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,13 +1,19 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState, useContext } from 'react'
 import { Menu } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 
+import { AuthContext } from '../context/auth'
+
 const Header = () => {
+  const { user, logout } = useContext(AuthContext);
 
-  const pathname = window.location.pathname;
-  const path = pathname === '/' ? 'home' : pathname.substring(1);
+  const [activeItem, setActiveItem] = useState('');
 
-  const [activeItem, setActiveItem] = useState(path)
+  useEffect(() => {
+    const pathname = window.location.pathname;
+    const path = pathname === '/' ? 'home' : pathname.substring(1);
+    setActiveItem(path)
+  },[user])
 
   const handleItemClick = (e, { name }) => setActiveItem(name)
 
@@ -20,22 +26,37 @@ const Header = () => {
         as={Link}
         to='/'
       />
-      <Menu.Menu position='right'>
-        <Menu.Item
-          name='login'
-          active={activeItem === 'login'}
-          onClick={handleItemClick}
-          as={Link}
-          to='/login'
-        />
-        <Menu.Item
-          name='register'
-          active={activeItem === 'register'}
-          onClick={handleItemClick}
-          as={Link}
-          to='/register'
-        />
-      </Menu.Menu>
+      { user === null ? (
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='login'
+            active={activeItem === 'login'}
+            onClick={handleItemClick}
+            as={Link}
+            to='/login'
+          />
+          <Menu.Item
+            name='register'
+            active={activeItem === 'register'}
+            onClick={handleItemClick}
+            as={Link}
+            to='/register'
+          />
+        </Menu.Menu>
+      ) : (
+        <Menu.Menu position='right'>
+          <Menu.Item
+            name='logout'
+            active={activeItem === 'logout'}
+            onClick={() => logout()}
+            as={Link}
+            to='/login'
+          />
+        </Menu.Menu>
+      )
+
+      }
+      
     </Menu>
   )
 }
