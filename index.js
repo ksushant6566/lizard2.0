7a81ecc112674d2cfcea8a582e0c2c5c89fb6c45
sushant <ksushant6566@gmail.com>
2021-04-09 01:18:53 +0530

    initial build

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ const resolvers = require('./graphql/resolvers');
 
 const pubsub = new PubSub();
 
+const PORT = process.env.PORT || 5000;
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
@@ -29,7 +31,7 @@ const server = new ApolloServer({
 mongoose.connect(ATLAS_URI, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("connected to database");
-        return server.listen({ port: 5000 })
+        return server.listen({ port: PORT })
     })
     .then(res => {
         console.log(`server running at ${res.url}`);
