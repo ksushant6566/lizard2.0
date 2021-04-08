a74a8314eff118fa4c6896c3e586af59aa7305e7
sushant <ksushant6566@gmail.com>
2021-04-09 01:24:40 +0530

    production

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    ATLAS_URI : 'mongodb+srv://ksushant6566:Maya%26shakti11@cluster0.nyghe.mongodb.net/Lizard?retryWrites=true&w=majority',
+    SECRET_KEY: 'oiashdfiohdoaishdaskdcnlkan'
+}
